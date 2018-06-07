51562b0e3e743d19f46e8419124bddf4f0881054
yongxianan <yxayxayxa2015@gmail.com>
2018-06-07 12:41:09 +0800

    correction for if statement bracket

--- src/triangle.c
+++ src/triangle.c
@@ -4,13 +4,13 @@
 
 char *getTriangleName(int line1 , int line2 ,int line3)
 {
-  if (line1<=0 || line2<=0 || line3<=0){
+  if ((line1<=0) || (line2<=0) || (line3<=0)){
     return "not triangle";
   }
-  else if (line1==line2 && line2==line3 && line3==line1){
+  else if ((line1==line2) && (line2==line3) && (line3==line1)){
     return "equilateral";
   }
-  else if (line1==line2 || line2==line3 || line3==line1){
+  else if ((line1==line2) || (line2==line3) || (line3==line1)){
     return "isosceles";
   }
   else{
